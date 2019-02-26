bf39849b7e9911a45ee5088f4cb2206ca5d36450
khayti08 <43528748+khayti08@users.noreply.github.com>
2019-02-25 17:02:56 -0700

    Add files via upload
    This is the first upload of the Calculator.java file. It includes the header but no other changes.

---
+++ Calculator.java
@@ -0,0 +1,41 @@
+// Name: Khyati Raka
+// Class ID: 530
+//Assignment number 2
+// Description: This program is designed to perform basic functions such as addition, subtraction, multiplication and division.
+
+
+
+package cse360assign2;
+
+public class Calculator {
+private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+
+}
